9807e4c0f732f3edf8e6d9224dcc7a047e81ea76
Leo Auri <code@leoauri.com>
2019-01-21 10:37:29 +0100

    Plot sine radially

---
+++ Assets/Scripts/Oscilloscope.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscilloscope : MonoBehaviour
+{
+    public GameObject scopePrefab;
+    private Transform scopePen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scopePen = Instantiate(scopePrefab, GetComponent<Transform>()).GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float osc = 2f;
+        //osc += 0.3f * Mathf.Sin(6.5f * Time.time); 
+        osc += 1f * Mathf.Sin(50f / 12f * Time.time);
+        scopePen.position = new Vector3(osc * Mathf.Sin(Time.time), osc * Mathf.Cos(Time.time));
+    }
+
+}
